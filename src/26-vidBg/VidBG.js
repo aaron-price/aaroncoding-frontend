c7478fc0419f53d32b53cf3efe4e47f9b945aa97
aaron-price <coding.aaronp@gmail.com>
2017-05-19 18:27:01 -0700

    Autoplay video backgrounds

--- src/26-vidBg/VidBG.js
+++ src/26-vidBg/VidBG.js
@@ -1,26 +1,33 @@
 import React from "react"
 import Paper from "material-ui/Paper"
 
-const VidBG  = props => {
-    return (
-        <div>
-            <Paper className="VidBG__text">
-                <p>This seems like a fitting background, no?</p><br/>
-                <p>The video quality is very poor because I had to shrink it signnificantly. Nobody wants to wait 5 minutes for a background to download.</p><br/>
-                <p>I'll keep an eye out for something a bit better</p>
-            </Paper>
-            <div className="VidBG__overlay"></div>
-            <video
-                className="VidBG__video"
-                autoPlay={true}
-            >
-                <source src="https://s3-us-west-2.amazonaws.com/aaroncoding/videos/bigCalendar.3gp" type="video/3gp"/>
-                <source src="https://s3-us-west-2.amazonaws.com/aaroncoding/videos/bigCalendar.mp4" type="video/mp4"/>
-                <source src="https://s3-us-west-2.amazonaws.com/aaroncoding/videos/bigCalendar.ogv" type="video/ogg"/>
-                <source src="https://s3-us-west-2.amazonaws.com/aaroncoding/videos/bigCalendar.webm" type="video/webm"/>
-            </video>
-        </div>
-    )
+class VidBG extends React.Component {
+    componentDidMount() {
+        let player = this.refs.player
+        setTimeout(() => player.play(), 300)
+    }
+    render() {
+        return (
+            <div>
+                <Paper className="VidBG__text">
+                    <p>This seems like a fitting background, no?</p><br/>
+                    <p>The video quality is very poor because I had to shrink it signnificantly. Nobody wants to wait 5 minutes for a background to download.</p><br/>
+                    <p>I'll keep an eye out for something a bit better</p>
+                </Paper>
+                <div className="VidBG__overlay"></div>
+                <video
+                    ref="player"
+                    className="VidBG__video"
+                    autoPlay={true}
+                >
+                    <source src="https://s3-us-west-2.amazonaws.com/aaroncoding/videos/bigCalendar.3gp"/>
+                    <source src="https://s3-us-west-2.amazonaws.com/aaroncoding/videos/bigCalendar.mp4" type="video/mp4"/>
+                    <source src="https://s3-us-west-2.amazonaws.com/aaroncoding/videos/bigCalendar.ogv" type="video/ogg"/>
+                    <source src="https://s3-us-west-2.amazonaws.com/aaroncoding/videos/bigCalendar.webm" type="video/webm"/>
+                </video>
+            </div>
+        )
+    }
 }
 
 export default VidBG
