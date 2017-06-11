8fdde150f63eec31f745fec4e4b0280359f18fb3
aaron-price <coding.aaronp@gmail.com>
2017-06-10 20:24:44 -0700

    Added test for accordion

--- src/ongoing/30Days/1-accordion/Accordion.test.js
+++ src/ongoing/30Days/1-accordion/Accordion.test.js
@@ -11,10 +11,16 @@ describe("30 days - accordion", () => {
     })
 
     test("contains the outer divs", () => {
-        // @TODO
+        expect(component.find(".accordion-item").length).toEqual(10)
+        expect(component.find(".accordion-item-0").length).toEqual(1)
     })
 
-    test("contains the inner divs", () => {
-        // @TODO
+    test("contains no inner divs if they're all hidden", () => {
+        expect(component.find(".accordion-inner").length).toEqual(0)
+    })
+
+    test("contains an inner div if it's visible", () => {
+        component.find(".accordion-outer-0").simulate("click")
+        expect(component.find(".accordion-inner").length).toEqual(1)
     })
 })
