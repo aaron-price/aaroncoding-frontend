cccb80115133137ead2c35f5b621e7b462fa45a4
aaron-price <coding.aaronp@gmail.com>
2017-06-17 22:14:29 -0700

    Implement menu toggle button

--- src/helpers/Menus/VertDrawer/VertDrawer.js
+++ src/helpers/Menus/VertDrawer/VertDrawer.js
@@ -5,13 +5,21 @@ import { connect } from "react-redux"
 import Paper from "material-ui/Paper"
 import { Link } from "react-router-dom"
 
-const defaultItems = ["title 1", "title 2", "title 3"]
+const defaultItems = ["title 1", "title 2", "title 3", "title 3", "title 3", "title 3"]
 
 const DefaultMenu = props => {
     const device = props.smallScreen ? "mobile" : "desktop"
+    const openStyle = {position: "fixed", top: 0, width: "100%"}
+    const closedStyle = {position: "fixed", top: -props.height, width: "100%"}
+    const desktopStyle = {}
+    let renderedStyle
+    if (props.smallScreen) {
+        renderedStyle = props.open ? openStyle : closedStyle
+    } else { renderedStyle = desktopStyle }
+
     return (
-        <div className={`default-menu-div-${device}`}>
-            <ul className={`default-menu-ul-${device}`}>
+        <div className={`default-menu-div-${device} default-menu-div`} style={renderedStyle}>
+            <ul className={`default-menu-ul-${device} default-menu-ul`}>
                 {props.items.map((item, key) => {
                     return (
                         <li className={`default-menu-li-${device}`} key={key}>
@@ -29,7 +37,7 @@ const DefaultMenu = props => {
                                 className="default-menu-toggle"
                                 secondary={true}
                                 onClick={() => {props.handleToggle()}}
-                                label={props.open.toString()} />
+                                label="MENU" />
                         </li>
                     )
                 }
@@ -44,8 +52,26 @@ class MenuContainer extends React.Component {
         super(props)
         this.state = {
             open: false,
+            height: 160,
         }
         this.handleToggle = this.handleToggle.bind(this)
+        this.getMenuHeight = this.getMenuHeight.bind(this)
+    }
+    componentDidMount() {
+        this.getMenuHeight()
+    }
+    getMenuHeight() {
+        const rect = document.getElementsByClassName("default-menu-div")[0].getBoundingClientRect()
+        const ul = document.getElementsByClassName("default-menu-ul")[0].getBoundingClientRect()
+        const toggle = document.getElementsByClassName("default-menu-toggle")[0].getBoundingClientRect()
+        const toggleHeight = toggle.bottom - toggle.top
+        const totalHeight = ul.bottom - ul.top
+        // const itemsLength = (this.props.items || defaultItems).length
+        // const heightPerItem = totalHeight / (itemsLength + 1)
+        // const toggleHeight = heightPerItem * itemsLength
+        console.log(totalHeight)
+        console.log(toggleHeight)
+        this.setState({height: totalHeight - toggleHeight - 20})
     }
     handleToggle = () => this.setState({open: !this.state.open})
     handleClose = () => this.setState({open: false})
@@ -55,6 +81,7 @@ class MenuContainer extends React.Component {
         return (
             <div>
                 <Menu
+                    height={this.state.height}
                     items={items}
                     smallScreen={this.props.smallScreen}
                     open={this.state.open}
@@ -75,6 +102,7 @@ DefaultMenu.propTypes = {
     open: PropTypes.bool.isRequired,
     items: PropTypes.array.isRequired,
     handleToggle: PropTypes.func.isRequired,
+    height: PropTypes.number.isRequired,
 }
 
 const mapStateToProps = (state) => {
