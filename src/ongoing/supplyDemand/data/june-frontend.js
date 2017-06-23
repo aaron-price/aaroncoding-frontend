980ef9fc57a201128a9e5b38e527a7c0f5b90912
aaron-price <coding.aaronp@gmail.com>
2017-06-22 19:56:49 -0700

    Update supply/demand data

---
+++ src/ongoing/supplyDemand/data/june-frontend.js
@@ -0,0 +1,79 @@
+const data = [
+    {
+        name: "react AND rails",
+        jobs: 25,
+        seekers: 87,
+    },
+    {
+        name: "react AND node",
+        jobs: 84,
+        seekers: 166,
+    },
+    {
+        name: "frontend (react or reactjs)",
+        jobs: 16,
+        seekers: 28,
+    },
+    {
+        name: "(frontend) (ruby or rails)",
+        jobs: 7,
+        seekers: 37,
+    },
+    {
+        name: "(frontend) (node nodejs)",
+        jobs: 10,
+        seekers: 28,
+    },
+    {
+        name: "(frontend) (mysql)",
+        jobs: 3,
+        seekers: 129,
+    },
+    {
+        name: "(frontend) (mongo or mongodb)",
+        jobs: 3,
+        seekers: 321,
+    },
+    {
+        name: "(frontend) (php)",
+        jobs: 5,
+        seekers: 120,
+    },
+    {
+        name: "(frontend) (js or javascript)",
+        jobs: 17,
+        seekers: 179,
+    },
+    {
+        name: "(frontend) (python)",
+        jobs: 3,
+        seekers: 69,
+    },
+    {
+        name: "(frontend) (python or django)",
+        jobs: 7,
+        seekers: 69,
+    },
+    {
+        name: "(node or nodejs)",
+        jobs: 119,
+        seekers: 117,
+    },
+    {
+        name: "(angular or angularjs)",
+        jobs: 178,
+        seekers: 89,
+    },
+    {
+        name: "(react or reactjs)",
+        jobs: 85,
+        seekers: 76,
+    },
+    {
+        name: "(ruby or rails)",
+        jobs: 215,
+        seekers: 210,
+    },
+]
+
+export default data
