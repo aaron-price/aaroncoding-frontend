21c9ff5de551c0506b852daa94db8dec9226ed95
aaron-price <coding.aaronp@gmail.com>
2017-06-23 07:18:15 -0700

    Update hire page

--- src/app/Hire/Hire.js
+++ src/app/Hire/Hire.js
@@ -7,22 +7,39 @@ const Hire = props => {
         <div className="hire-wrapper">
 
             <Paper className="padded-paper hire-header-wrapper">
-                <div className="padded-paper hire-header-image-wrapper" style={{minHeight: "250px", minWidth: "250px"}}>
+                <div className="padded-paper hire-header-image-wrapper">
                     <img
                         alt="Aaron Price"
+                        style={{height: "40vmin", width: "40vmin"}}
                         className="hire-header-image"
                         src="https://s3-us-west-2.amazonaws.com/aaroncoding/images/me1.png"/>
                 </div>
-                <div className="padded-paper hire-header-text-wrapper" style={{minHeight: "250px", minWidth: "250px"}}>
+                <div className="padded-paper hire-header-text-wrapper">
                     <div className="hire-header-text">
                         <h1>Hire Aaron Price</h1>
                         <h3>Frontend UX/UI Developer</h3>
                         <p>I make web apps come alive. My top skills are:</p>
                         <ul>
-                            <li>Javascript - React, Node, NoSql Databases</li>
-                            <li>CSS - SCSS, postCSS</li>
-                            <li>HTML </li>
-                            <li>Ruby on Rails</li>
+                            <li>Functional Programming - 70% expertise</li>
+                            <li>Object Oriented Programming - 70% expertise (prototypal preferred)</li>
+                            {/*
+                                <li>Cryptography - 30% expertise</li>
+                                <li>Algorithms - 30% expertise</li>
+                                <li>Machine Learning - 0% expertise</li>
+                            */}
+                            <li><hr/></li>
+                            <li>Javascript - 90% expertise</li>
+                            <li>React/redux - 90% expertise</li>
+                            <li>NodeJS/express - 60% expertise</li>
+                            <li><hr/></li>
+                            <li>CSS and HTML 85% expertise</li>
+                            <li>SCSS and postCSS 60% expertise</li>
+                            <li><hr/></li>
+                            <li>Ruby 60% expertise</li>
+                            <li>Rails 60% expertise</li>
+                            <li><hr/></li>
+                            <li>MySQL 50% expertise</li>
+                            <li>MongoDB 50% expertise</li>
                         </ul>
                     </div>
                 </div>
