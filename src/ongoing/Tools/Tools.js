cd5f9f37bda4cad92d26ef3cb49be93e412b458c
Aaron Price <coding.aaronp@gmail.com>
2017-09-15 18:45:47 -0700

    Add Reactjo to /tools

--- src/ongoing/Tools/Tools.js
+++ src/ongoing/Tools/Tools.js
@@ -4,25 +4,28 @@ import { Link } from "react-router-dom"
 
 const items = [
     {
+        title: "Reactjo",
+        description: "Scaffolding engine. Extensible and flexible, providing Django backend, NodeJS frontend. User authentication and model CRUD.",
+        uri: "https://github.com/aaron-price/reactjo",
+        image: "https://s3-us-west-2.amazonaws.com/aaroncoding/images/crane.png",
+    },
+    {
         title: "Sweet Render",
         description: "An open source template engine sandbox. Can be used to create your own alternative syntax to React's 'JSX' syntax.",
         uri: "https://www.npmjs.com/package/sweet-render",
         image: "https://s3-us-west-2.amazonaws.com/aaroncoding/images/sweet_render.png",
-    },{
-        title: "Developer quiz",
-        description: "A quiz I update constantly with questions ranging from html, css, javascript, sql, nosql, tdd/bdd, and more.",
-        uri: "/quiz",
-        image: "https://s3-us-west-2.amazonaws.com/aaroncoding/images/quiz.png",
-    },{
+    },
+    {
         title: "Supply and demand",
         description: "I take a data driven approach to make myself as useful as possible to potential employers. Here's the developer supply:demand data I've collected.",
         uri: "/supply_and_demand",
         image: "https://s3-us-west-2.amazonaws.com/aaroncoding/images/supply_demand.png",
-    },{
-        title: "Stock validation",
-        description: "I examine various stocks using criteria from Benjamin Graham, Warren Buffet, and check the piotroski f-score",
-        uri: "/stocks",
-        image: "https://s3-us-west-2.amazonaws.com/aaroncoding/images/stocks.jpg",
+    },
+    {
+        title: "Developer quiz",
+        description: "A quiz I update constantly with questions ranging from html, css, javascript, sql, nosql, tdd/bdd, and more.",
+        uri: "/quiz",
+        image: "https://s3-us-west-2.amazonaws.com/aaroncoding/images/quiz.png",
     },
 ]
 
