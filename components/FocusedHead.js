7a3b6106ac35e8c7114264a999dd112f0b087a67
Aaron Price <coding.aaronp@gmail.com>
2017-10-29 13:33:26 -0700

    Add verification tag for search console

--- components/FocusedHead.js
+++ components/FocusedHead.js
@@ -13,17 +13,20 @@ const muiTheme = getMuiTheme({ userAgent: false })
 export default (props) => (
     <MuiThemeProvider muiTheme={muiTheme}>
         <div>
+            <div id='top'></div>
             <Head>
                 <title>Aaron Price, Web Developer</title>
                 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossOrigin="anonymous"/>
                 <meta name="viewport" content='width=device-width, initial-scale=1' />
                 <meta httpEquiv="Content-Type" content='text/html; charset=utf-8' />
+                <meta name="google-site-verification" content="Z3EvB2HyL8WXW9eD9wlCv729jr1sqka-i0sCjBT1gkI" />
                 <meta name="description" content={ props.description || 'Portfolio website of a front-leaning web developer. Javascript and React abound'} />
-                <script dangerouslySetInnerHTML={{__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','GTM-K9SDT53');`}} />
+                <script dangerouslySetInnerHTML={{
+                    __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                    })(window,document,'script','dataLayer','GTM-K9SDT53');`}} />
             </Head>
             <noscript dangerouslySetInnerHTML={{__html: '<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K9SDT53" height="0" width="0" style="display:none;visibility:hidden;"></iframe>'}} />
             <style dangerouslySetInnerHTML={{ __html: stylesheet }} />
