31b673639b1ef6f981faa6cb8b3cab8e80ef44e4
Aaron Price <coding.aaronp@gmail.com>
2017-10-29 13:34:24 -0700

    Remove superfluous div

--- components/FocusedHead.js
+++ components/FocusedHead.js
@@ -13,7 +13,6 @@ const muiTheme = getMuiTheme({ userAgent: false })
 export default (props) => (
     <MuiThemeProvider muiTheme={muiTheme}>
         <div>
-            <div id='top'></div>
             <Head>
                 <title>Aaron Price, Web Developer</title>
                 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossOrigin="anonymous"/>
