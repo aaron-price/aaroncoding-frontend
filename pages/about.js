31634a83e66a970e5cc92948967cc6428c08d383
Aaron Price <coding.aaronp@gmail.com>
2017-11-04 15:05:15 -0700

    Colorful /about

--- pages/about.js
+++ pages/about.js
@@ -30,7 +30,7 @@ const Text = (props) => (
                     <Handshake />
                 </div>
                 <div className='about_textdiv'>
-                    <h4>Hire Me</h4>
+                    <h4 className='about_header'>Hire Me</h4>
                     <p>Email: coding.aaronp@gmail.com <br/>
                                     Phone: 604-996-8835</p>
                     <p>Or fill out this form</p>
@@ -45,7 +45,7 @@ const Text = (props) => (
                     <Education />
                 </div>
                 <div className='about_textdiv'>
-                    <h4>Education</h4>
+                    <h4 className='about_header'>Education</h4>
                     <p>2010 - Certificate in web design and animation from <a href='https://www.ufv.ca/'>UFV</a>.</p>
                 </div>
             </Paper><br/>
@@ -57,7 +57,7 @@ const Text = (props) => (
                     <Octocat />
                 </div>
                 <div className='about_textdiv'>
-                    <h4>Open Source</h4>
+                    <h4 className='about_header'>Open Source</h4>
                     <p>According to GitHub, I made <a href='https://github.com/aaron-price/'><strong>over 2,000</strong> contributions</a> in the last year.</p>
                     <p>My main projects:</p>
                     <ul className='unstyled_list'>
@@ -74,7 +74,7 @@ const Text = (props) => (
                     <Bug />
                 </div>
                 <div className='about_textdiv'>
-                    <h4>Problem Solving</h4>
+                    <h4 className='about_header'>Problem Solving</h4>
                     <p>I used to do customer service - mostly tech support.</p>
                     <p>During that 4.5 year period, I solved <strong>over 120,000</strong> cases.</p>
                     <p>I was heavily involved in writing the tech support guidelines, and training other staff.</p>
@@ -90,7 +90,7 @@ const Text = (props) => (
                     <Js />
                 </div>
                 <div className='about_textdiv'>
-                    <h4>Technologies</h4>
+                    <h4 className='about_header'>Technologies</h4>
                     <p>Languages:</p>
                     <Skill value={8.5}>Javascript</Skill>
                     <Skill value={8}>HTML</Skill>
@@ -118,15 +118,15 @@ const Text = (props) => (
                     <img src={smile} className='about_img__me' />
                 </div>
                 <div className='about_textdiv'>
-                    <h4>My Backstory</h4>
-                    <p><strong>Nerdy before it was cool.</strong></p>
+                    <h4 className='about_header'>My Backstory</h4>
+                    <p><strong className='about_strong'>Nerdy before it was cool.</strong></p>
                     <p> I remember fiddling with hex codes
                         in my gameboy gameshark as a child, to hack pokemon (the original one).
                         And yes, I <i>did</i> catch them all.<br/><br/>
                         Then there was lego mindstorms which lets you build and program robots.
                         I did farm labour all summer to save up for it when I was 9 or 10. Totally worth it.</p>
 
-                    <p><strong>University.</strong></p>
+                    <p><strong className='about_strong'>University.</strong></p>
                     <ol>
                         <li>I skipped a grade and went to university at age 17</li>
                         <li>The great recession decimated the job market</li>
@@ -134,7 +134,7 @@ const Text = (props) => (
                         <li>I did manual labour and tech support for the next several years</li>
                     </ol>
 
-                    <p><strong>Becoming a web developer.</strong></p>
+                    <p><strong className='about_strong'>Becoming a web developer.</strong></p>
                     <p>August 2016 - I decided (again) to become a web developer, at all costs.
                         So I started teaching myself after work, and have learned or built something
                         pretty much every day, ever since.
@@ -151,7 +151,9 @@ const Text = (props) => (
                         a site with a Django backend app and a Node frontend app, rendering React,
                         with user authentication, permissions, and feature rich scaffolding.
                     </p>
+                    <p><strong className='about_strong'>Present</strong></p>
                     <p>October 2017 - I rebuilt my portfolio site using my scaffolding engine and I've been freelancing ever since.</p>
+                    <p>Eventually I would like to get a full time job as a web developer.</p>
                 </div>
             </Paper>
         </div><br/>
