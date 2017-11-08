538fc58ebd2e96db9614dd7ac56eeb46275d8140
Aaron Price <coding.aaronp@gmail.com>
2017-11-08 15:21:51 -0800

    Remove calendar page

--- pages/freelance/calendar.js
+++
@@ -1,5 +0,0 @@
-import Calendar from '../../components/Freelance/Calendar/Calendar.js'
-import FocusedHead from '../../components/FocusedHead.js'
-export default (props) => {
-    return <FocusedHead><div style={{padding: '2em'}}><Calendar /></div></FocusedHead>
-}
